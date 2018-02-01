fec2ae97bbfb668bbe5c71137c08bd787fb91cbd
zengbailiang <zengbailiang@zhangzydemacbook-pro.local>
2018-02-01 23:52:38 +0800

    Initial Commit

---
+++ SafeNetDataProcessor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SafeNetDataProcessor
+//
+//  Created by zengbailiang on 2018/2/1.
+//  Copyright © 2018年 zengbailiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SafeNetDataProcessorTests/SafeNetDataProcessorTests.m
@@ -0,0 +1,39 @@
+//
+//  SafeNetDataProcessorTests.m
+//  SafeNetDataProcessorTests
+//
+//  Created by zengbailiang on 2018/2/1.
+//  Copyright © 2018年 zengbailiang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SafeNetDataProcessorTests : XCTestCase
+
+@end
+
+@implementation SafeNetDataProcessorTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
